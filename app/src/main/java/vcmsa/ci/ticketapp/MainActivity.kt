a1b7e12709c5c7a077f6c039590002999087e69a
lab_services_student <st10479750@vcconnect.edu.za>
2025-04-02 18:36:04 +0200

    Ticket App

---
+++ app/src/main/java/vcmsa/ci/ticketapp/MainActivity.kt
@@ -0,0 +1,40 @@
+package vcmsa.ci.ticketapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val speedInput = findViewById<EditText>(R.id.speedInput)
+        val checkButton = findViewById<Button>(R.id.checkBtn)
+
+        checkButton.setOnClickListener {
+            val speed = speedInput.text.toString().toIntOrNull()
+
+            if (speed == null) {
+                Toast.makeText(this, "You not even driving!", Toast.LENGTH_SHORT).show()
+            } else if (speed < 60) {
+                Toast.makeText(this, "You are driving safely!", Toast.LENGTH_SHORT).show()
+            } else if (speed <= 80) {
+                Toast.makeText(this, "Warning! Drive carefully!", Toast.LENGTH_SHORT).show()
+            } else if (speed <= 100) {
+                Toast.makeText(this, "Fine: R500", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Fine: R2000", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
